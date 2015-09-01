32f998b49d8cfcf0fde8a1f0ef805933657e74a4
Denis Salmanovich <denis.salmanovich@gmail.com>
2015-09-01 14:30:16 +0300

    .

--- jnotes/src/jnotes/Main.java
+++ jnotes/src/jnotes/Main.java
@@ -4,35 +4,26 @@ import java.awt.*;
 import java.awt.event.*;
 import java.io.*;
 import java.net.URL;
+import java.util.ArrayList;
 
 import javax.swing.*;
 
 
 public class Main {
+	final static String userNotesPath = System.getProperty("user.home")+"/.jnotes";
+	final static String userNotesDataPath = System.getProperty("user.home")+"/.jnotes/data";
+	final static PopupMenu popup = new PopupMenu();
+	final static TrayIcon trayIcon = new TrayIcon(createImage("resources/icon.png", "tray icon"));
+	final static SystemTray tray = SystemTray.getSystemTray();
+	final static int[] myArray = new int[2];
+	
+	public static int index;
 	
 	public static void main(String[] args) {
-		// Проверяем созданы ли все директории, если нет, то создаем
-		final String userNotesPath = System.getProperty("user.home")+"/.jnotes";
-		final String userNotesDataPath = System.getProperty("user.home")+"/.jnotes/data";
-		
-		File userNotesDir = new File(userNotesPath);
-		if (!userNotesDir.exists()) {
-		    System.out.println("creating directory: " + userNotesPath);
-		    userNotesDir.mkdir();
-		}
-		
-		File userNotesDataDir = new File(userNotesDataPath);
-		if (!userNotesDataDir.exists()) {
-		    System.out.println("creating directory: " + userNotesDataPath);
-		    userNotesDataDir.mkdir();
-		}
-		
+		initDataDir();
 		
 		// Помещаем иконку в трэе
-		final TrayIcon trayIcon = new TrayIcon(createImage("resources/icon.png", "tray icon"));
-		final SystemTray tray = SystemTray.getSystemTray();
 		trayIcon.setImageAutoSize(true);
-        
         try {
             tray.add(trayIcon);
         } catch (AWTException e) {
@@ -41,8 +32,6 @@ public class Main {
         }
         
         // Create a default popup menu components
-        final PopupMenu popup = new PopupMenu();
-        
         MenuItem newNote = new MenuItem("New note");
         MenuItem optionsItem = new MenuItem("Options");
         MenuItem exitItem = new MenuItem("Exit");
@@ -51,34 +40,8 @@ public class Main {
         popup.addSeparator();
         
         // Динамическое меню
-        
-        String noteFileName;
-        String mnu;
-        
-        File folder = new File(userNotesPath+"/data");
-    	File[] listOfFiles = folder.listFiles();
-    	for (int i = 0; i < listOfFiles.length; i++) {
-    		if (listOfFiles[i].isFile()) {
-    			noteFileName = listOfFiles[i].getName();
-    			File f = new File(userNotesPath+"/data/"+noteFileName);
-    			try {
-					BufferedReader fin = new BufferedReader(new FileReader(f));
-					try {
-						// mnu = первоя строка заметки
-						mnu = fin.readLine();
-						MenuItem menuItem = new MenuItem (mnu);
-						menuItem.setName("dsf");
-						popup.add(menuItem);
-					} catch (IOException e1) {
-						e1.printStackTrace();
-					}
-				} catch (FileNotFoundException e1) {
-					e1.printStackTrace();
-				}
-    		}
-    	}
-    	
-    	
+        dynamicMenu();
+          
         popup.addSeparator();
         popup.add(optionsItem);
         popup.addSeparator();
@@ -86,13 +49,6 @@ public class Main {
         
         trayIcon.setPopupMenu(popup);
         
-        // Проверка
-        popup.getItem(0).addActionListener(new ActionListener() {
-        	public void actionPerformed(ActionEvent e) {
-        		System.out.println(popup.getItem(0).getName());
-        	}
-        });
-
         trayIcon.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 //JOptionPane.showMessageDialog(null,"Управление правой кнопкой мышки!");
@@ -109,9 +65,52 @@ public class Main {
             }
         });
         
+        for (int g=0; g<2; g++) {
+        	index = g+2;
+        	System.out.println(index+"---"+popup.getItem(index).getName());
+        	popup.getItem(index).addActionListener(new ActionListener() {
+        		public void actionPerformed(ActionEvent e) {
+            		System.out.println(popup.getItem(index).getName());
+            	}
+            });
+        }
 	}
 	
-	//Obtain the image URL
+	protected static void dynamicMenu() {
+		String noteFileName;
+        String noteName;
+        
+        
+        File folder = new File(userNotesPath+"/data");
+    	File[] listOfFiles = folder.listFiles();
+    	
+
+    	for (int i = 0; i < listOfFiles.length; i++) {
+    		if (listOfFiles[i].isFile()) {
+    			noteFileName = listOfFiles[i].getName();
+    			File f = new File(userNotesPath+"/data/"+noteFileName);
+    			try {
+					BufferedReader fin = new BufferedReader(new FileReader(f));
+					try {
+						// mnu = первоя строка заметки т.е будет как ее заголовок
+						noteName = fin.readLine();
+						MenuItem menuItem = new MenuItem (noteName);
+						menuItem.setName(noteFileName);
+						popup.add(menuItem);
+						
+						myArray[i] = i+2;
+						
+					} catch (IOException e1) {
+						e1.printStackTrace();
+					}
+				} catch (FileNotFoundException e1) {
+					e1.printStackTrace();
+				}
+    		}
+    	}
+	}
+	
+	// Obtain the image URL
     protected static Image createImage(String path, String description) {
         URL imageURL = Main.class.getResource(path);
         
@@ -121,5 +120,20 @@ public class Main {
         } else {
             return (new ImageIcon(imageURL, description)).getImage();
         }
+    }
+    
+    protected static void initDataDir() {
+    	// Проверяем созданы ли все директории, если нет, то создаем
+		File userNotesDir = new File(userNotesPath);
+		if (!userNotesDir.exists()) {
+		    System.out.println("creating directory: " + userNotesPath);
+		    userNotesDir.mkdir();
+		}
+		
+		File userNotesDataDir = new File(userNotesDataPath);
+		if (!userNotesDataDir.exists()) {
+		    System.out.println("creating directory: " + userNotesDataPath);
+		    userNotesDataDir.mkdir();
+		}
     }
 }
