3b5296e6ae9d6f503add65b8109167327a99e3ef
Denis Salmanovich <denis.salmanovich@gmail.com>
2015-09-02 12:51:42 +0300

    Работает дин меню

--- jnotes/src/jnotes/Main.java
+++ jnotes/src/jnotes/Main.java
@@ -4,7 +4,6 @@ import java.awt.*;
 import java.awt.event.*;
 import java.io.*;
 import java.net.URL;
-import java.util.ArrayList;
 
 import javax.swing.*;
 
@@ -15,9 +14,6 @@ public class Main {
 	final static PopupMenu popup = new PopupMenu();
 	final static TrayIcon trayIcon = new TrayIcon(createImage("resources/icon.png", "tray icon"));
 	final static SystemTray tray = SystemTray.getSystemTray();
-	final static int[] myArray = new int[2];
-	
-	public static int index;
 	
 	public static void main(String[] args) {
 		initDataDir();
@@ -64,27 +60,16 @@ public class Main {
                 System.exit(0);
             }
         });
-        
-        for (int g=0; g<2; g++) {
-        	index = g+2;
-        	System.out.println(index+"---"+popup.getItem(index).getName());
-        	popup.getItem(index).addActionListener(new ActionListener() {
-        		public void actionPerformed(ActionEvent e) {
-            		System.out.println(popup.getItem(index).getName());
-            	}
-            });
-        }
 	}
+	
 	
 	protected static void dynamicMenu() {
 		String noteFileName;
         String noteName;
         
-        
         File folder = new File(userNotesPath+"/data");
     	File[] listOfFiles = folder.listFiles();
     	
-
     	for (int i = 0; i < listOfFiles.length; i++) {
     		if (listOfFiles[i].isFile()) {
     			noteFileName = listOfFiles[i].getName();
@@ -92,14 +77,18 @@ public class Main {
     			try {
 					BufferedReader fin = new BufferedReader(new FileReader(f));
 					try {
-						// mnu = первоя строка заметки т.е будет как ее заголовок
+						// noteName = первоя строка заметки т.е будет как ее заголовок
 						noteName = fin.readLine();
-						MenuItem menuItem = new MenuItem (noteName);
+						final MenuItem menuItem = new MenuItem (noteName);
 						menuItem.setName(noteFileName);
 						popup.add(menuItem);
 						
-						myArray[i] = i+2;
-						
+						menuItem.addActionListener(new ActionListener() {
+							@Override
+							public void actionPerformed(ActionEvent e) {
+			            		System.out.println(menuItem.getName());
+			            	}
+			            });
 					} catch (IOException e1) {
 						e1.printStackTrace();
 					}
