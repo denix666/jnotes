45eb564c165430fcbe5b4d80299df6035b36a925
Denis Salmanovich <denis.salmanovich@gmail.com>
2015-09-03 16:37:01 +0300

    s

--- jnotes/src/jnotes/Note.java
+++ jnotes/src/jnotes/Note.java
@@ -1,51 +1,93 @@
 package jnotes;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
-import java.awt.Font;
+import java.awt.Point;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 
-import javax.swing.BorderFactory;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
+import javax.swing.ScrollPaneConstants;
+import javax.swing.border.EtchedBorder;
+import javax.swing.border.TitledBorder;
 
 
-public class Note extends JPanel {
-
+public class Note extends JPanel{
+	
 	JFrame frame = new JFrame();
-	JTextArea textArea = new JTextArea();
-	JScrollPane textAreaScrollPane = new JScrollPane(textArea);
+	JPanel middlePanel = new JPanel();
+	JTextArea display = new JTextArea(20,40);
+	String noteName = null;
+	String data = null;
+	String framePosX, framePosY = null;
 	
 	public Note(String noteFileName) {
-		frame.add(this);
-		frame.setSize(300, 300);
-		frame.setTitle(noteFileName);
-		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		frame.setLocationRelativeTo(null);
-		frame.setVisible(true);
-        
-		textArea.setLineWrap(true);
-		textArea.setWrapStyleWord(true);
-		textArea.setFont(new Font("Serif", Font.ITALIC, 13));
-		textArea.setBackground(new Color(204, 238, 241));
-		
-			
-		textAreaScrollPane.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
-		textAreaScrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
-		
-		textArea.add(textAreaScrollPane);
-		
-		this.add(textArea);
 		
 		
-	
-		
-		
-		//frame.setLocationRelativeTo(null);
-		//frame.setBounds(100, 100, 450, 300);
-		//frame.setTitle("ku");
-		//frame.setBackground(Color.ORANGE);
-		//frame.setResizable(true);
-		//frame.setUndecorated(true);
+		final File note = new File(Main.userNotesPath+"/data/"+noteFileName);
+		try {
+			BufferedReader br = new BufferedReader(new FileReader(note));
+			try {
+				noteName = br.readLine();
+				framePosX = br.readLine();
+				framePosY = br.readLine();
+				while ((data = br.readLine()) != null) {
+					display.append(data+"\n");
+				}
+			} catch (IOException e1) {
+				e1.printStackTrace();
+			}
+		} catch (FileNotFoundException e1) {
+			e1.printStackTrace();
+		}
+
+
+	    middlePanel.setBorder(new TitledBorder(new EtchedBorder(),noteName));
+	    middlePanel.setLayout(new BorderLayout());
+	    
+	    display.setEditable(true);
+	    display.setBackground(Color.ORANGE);
+	    JScrollPane scroll = new JScrollPane(display);
+	    scroll.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+	    middlePanel.add (scroll,BorderLayout.CENTER);
+	    frame.add(middlePanel);
+	    frame.pack();
+	    int x = Integer.parseInt(framePosX);
+	    int y = Integer.parseInt(framePosY);
+	    frame.setLocation(x, y);
+	    frame.setVisible(true);
+	    
+	    
+	    frame.addWindowListener(new WindowAdapter() {
+
+	    	@Override
+            public void windowClosing(WindowEvent e) {
+	    		
+	    		framePosX=frame.getX();
+	    		
+	    		FileWriter fileWriter;
+				try {
+					fileWriter = new FileWriter(note);
+					fileWriter.write(noteName+"\n");
+					fileWriter.append(frame.getX());
+					fileWriter.append(frame.getY());
+					fileWriter.append(display.getText());
+					fileWriter.close();
+				} catch (IOException e1) {
+					e1.printStackTrace();
+				}
+            }
+        });
 	}
 }
